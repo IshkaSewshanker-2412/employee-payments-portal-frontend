32484d256a0eba09b2e4f31a8a7ed20654cb8c95
Ishka Sewshanker <109598715+IshkaSewshanker-2412@users.noreply.github.com>
2026-05-24 17:10:20 +0200

    Fix test to match Employee Payments Portal heading

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Employee Payments Portal heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/Employee Payments Portal/i);
+  expect(headingElement).toBeInTheDocument();
 });
+
